d9d15c448369e7615092912aee693567edda98dd
Himanshu Pandey <himanshupandey7860988150@gmail.com>
2025-11-16 15:17:12 +0530

    first app created

---
+++ Next.js/codeWH/code/first-app/app/contact/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+// import Navbar from '@/components/Navbar'
+const contact = () => {
+    return (
+        <div>
+            {/* <Navbar/> */}
+            <h3>This is contact page</h3>
+        </div>
+    )
+}
+
+export default contact
---
+++ Next.js/codeWH/code/first-app/components/Navbar.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Navbar = () => {
+    return <div>
+        I am a navbar
+    </div>;
+};
+
+export default Navbar;
